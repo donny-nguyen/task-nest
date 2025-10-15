9f5a24abb12071207619f22d5d6a709d33f4f88d
Dung Nguyen <donny.nguyen.swe@outlook.com>
2025-10-14 22:43:54 -0400

    Add get all tasks function

---
+++ backend/getAllTasks.js
@@ -0,0 +1,15 @@
+// getAllTasks.js
+const AWS = require('aws-sdk');
+const dynamo = new AWS.DynamoDB.DocumentClient();
+const TABLE_NAME = 'Tasks';
+
+exports.handler = async () => {
+  const params = {
+    TableName: TABLE_NAME,
+  };
+  const data = await dynamo.scan(params).promise();
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data.Items || []),
+  };
+};
