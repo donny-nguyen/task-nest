7b993c803db92c8a14c0473e81bff5ac9c7ef22b
Dung Nguyen <donny.nguyen.swe@outlook.com>
2025-10-18 15:30:37 -0400

    Using built-in crypto instead of uuid package

--- backend/createTask.js
+++ backend/createTask.js
@@ -1,13 +1,13 @@
+import { randomUUID } from 'crypto';
 import { DynamoDBClient, PutItemCommand, UpdateItemCommand } from '@aws-sdk/client-dynamodb';
-import { marshall, unmarshall } from '@aws-sdk/util-dynamodb';
-import { v4 as uuidv4 } from 'uuid';
+import { marshall } from '@aws-sdk/util-dynamodb';
 
 const client = new DynamoDBClient({});
 const TABLE_NAME = process.env.TABLE_NAME;
 
 export const handler = async (event) => {
   const body = JSON.parse(event.body);
-  const taskId = uuidv4();
+  const taskId = randomUUID();
 
   const item = {
     TaskID: taskId,
